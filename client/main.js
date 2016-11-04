9640178939f980d72a290c2382124bdb6076060b
manon <manonbehr@gmail.com>
2016-11-04 22:13:27 +0100

    responsive menu

--- client/main.js
+++ client/main.js
@@ -5,3 +5,22 @@ import './main.html';
 import './posts/ListePosts.js';
 
 
+$(function() {
+	$(window).resize(function(){
+		var w = $(window).width();
+		if (w > 850){
+			$('.menu_mobile').hide();
+		}
+	});
+	// Mobile Navigation mouais
+	// $('.bt_menu_mobile').click(function() {
+	//     if ($('.menu').hasClass('menu')) {
+	//         $('.menu').addClass('menu_mobile');
+	//         $('.menu').removeClass('menu');
+	//     } else {
+	//     	$('.menu_mobile').addClass('menu');
+	//     	$('menu').removeClass('menu_mobile');
+	//     }
+	// });
+
+});
